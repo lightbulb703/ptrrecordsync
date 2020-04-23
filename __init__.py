999f609f1cd8d46c6b444a3996c418be112c4cce
Dennis Cole III <dennis@lbsys.xyz>
2020-04-23 14:28:29 -0400

    Update README. Update code using keyword args. Added sample conf and systemd files

---
+++ __init__.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+__author__ = 'Dennis Cole III'
+__copyright__ = 'Copyright 2020, Lightbulb Systems LLC'
+__license__ = 'GPLv3'
+__version__ = '0.1.0'
+__maintainer__ = 'Dennis Cole III'
+__email__ = 'dcole@netatwork.com'
+__status__ = 'Developing'
+
+all = ['__author__',
+       '__copyright__',
+       '__license__',
+       '__version__',
+       '__maintainer__',
+       '__email__',
+       '__status__'
+       ]
