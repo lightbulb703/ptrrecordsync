ea8dfb0fca73cd3a786a8cd5d061f29fae00717d
Dennis Cole III <dennis@lbsys.xyz>
2020-05-01 14:53:41 -0400

    Update

--- ptrrecordsync/__init__.py
+++ ptrrecordsync/__init__.py
@@ -4,7 +4,7 @@ __license__ = 'GPLv3'
 __version__ = '0.2.1'
 __maintainer__ = 'Dennis Cole III'
 __email__ = 'dennis@lbsys.xyz'
-__status__ = 'Production'
+__status__ = 'Development'
 __url__ = 'https://github.com/lightbulb703/ptrrecordsync.git'
 
 all = ['__author__',
